a75d7e03fa30ed743b67ad2dcb7bc22ba5fbcf63
madina-akd <akadimadina0@gmail.com>
2025-12-06 12:15:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,20 @@ require __DIR__.'/front.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/api.php';
 
+Route::get('/test-mail', function () {
+    $validated = [
+        'prenom' => 'Test',
+        'nom' => 'Contact',
+        'email' => 'akadimadina0@gmail.com',
+        'sujet' => 'test',
+        'message' => 'grace test'
+    ];
+
+    try {
+        Mail::to('akadimadina0@gmail.com')->send(new \App\Mail\ContactMessage($validated));
+        dd('Mail envoyé'); // test réussi
+    } catch (\Exception $e) {
+        dd($e->getMessage()); // affichera l’erreur SMTP si ça échoue
+    }
+});
 
